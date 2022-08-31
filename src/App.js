9a99dc615f905afd9845667d5774d5d7605e3fa0
KindEni <kind_official@outlook.com>
2022-08-31 10:31:43 -0700

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import  Typography  from '@mui/material';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant='h2' component='h2' data-test='heading'>
+        Boookish
+      </Typography>
+    </div>
+  );
+}
+
+export default App;

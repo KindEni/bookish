661988d4124246d5583572c4708c39212c24caed
KindEni <kind_official@outlook.com>
2022-09-02 11:16:56 -0700

    Finished test implementation for the book list

--- src/BookList.js
+++ src/BookList.js
@@ -1,14 +1,13 @@
-const BookList = ({ books, loading, error }) => {
+import React from "react";
+
+const BookList = ({ loading, books, error }) => {
+  if (loading) { return <p>Loading</p> };
+  if (error) { return <p>Error</p> };
 
   return <div data-test='book-list'>
-    
-    {loading || error ? (<span>{error || 'Loading...'}</span>) :
-      (books.map(book => (<div className='book-item'>
-        <h2 className='title'>{book.name}</h2>
-      </div>))
-      )}
-  </div>
+    {books.map(book => (<div className='book-item' key={book.id}>
+      <h2 className='title'>{book.name}</h2></div>))}
+  </div>;
 }
 
-export default BookList;
-
+export default BookList
